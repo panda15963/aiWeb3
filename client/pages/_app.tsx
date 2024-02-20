936cd8141b2041cef1f07218ddbfead9252a7aa9
MinseokChoi <seok7521@gmail.com>
2024-02-20 17:23:02 +0900

    sigin feature

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,6 +1,7 @@
+import React from 'react';
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import {NextUIProvider} from '@nextui-org/react';
+import { NextUIProvider } from '@nextui-org/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from './components/navbar';
 import Footer from "./components/Footer";
+import 'tailwindcss/tailwind.css';
 export default function App() {  ;
   return (
     <div>
