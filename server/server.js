222e4c926bd9627854dadeefc32f6d529b2686b5
minseok choi <seok7521@gmail.com>
2024-03-06 14:28:21 +0900

    collecting Ethereum values from Backend to frontend

--- server/server.js
+++ server/server.js
@@ -1,10 +1,28 @@
+const WebSocket = require('ws');
 const express = require('express');
-const app = express();
+const http = require('http');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+let clientSocket;
+
+wss.on('connection', function connection(ws) {
+  clientSocket = ws;
+  console.log('WebSocket connected');
+  
+  // Coinbase Pro WebSocket 엔드포인트 URL
+  const wsUrl = 'wss://ws-feed.pro.coinbase.com';
+  const coinbaseSocket = new WebSocket(wsUrl);
+  coinbaseSocket.on('message', function incoming(data) {
+    // 메시지를 프론트엔드로 전달
+    if (clientSocket.readyState === WebSocket.OPEN) {
+      clientSocket.send(data);
+    }
+  });
 });
 
-app.listen(3001, () => {
-  console.log('Example app listening on port 3001!');
+server.listen(3000, function() {
+  console.log('Backend server listening on port 3000');
 });
